621a5871a82ee36927e0ff59b731487f3f21cd4e
dmacompton <d.mihaylov@ringostat.com>
2016-03-14 08:27:01 +0200

    43

---
+++ src/actions/index.js
@@ -0,0 +1,4 @@
+export function selectBook(book) {
+    console.log('Selected: ', book.title);
+    // return { }
+}
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        { title: 'JS: The Good Parts'},
+        { title: 'Harry Potter'},
+        { title: 'C++'},
+        { title: 'HTML & CSS'}
+    ]
+}
